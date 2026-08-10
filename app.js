d3aa1a3a000990059e1a6f6a5f0a0aa5f2e518f9
muhammadnoumanx <mnoumanarshad01@gmail.com>
2026-08-10 18:10:30 +0500

    Fix duplicate port error

--- app.js
+++ app.js
@@ -51,7 +51,6 @@ app.use((req, res) => {
   });
 });
 app.use(errorHandler);
-const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
